d747b1cea88f77a8865627a5e7a3b8d0e1883d47
Meryl Turner <merylturner08@gmail.com>
2017-08-31 14:35:09 -0700

    initial setup of konva components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Layer, Rect, Stage, Group } from 'react-konva';
+
+class MyRect extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      color: 'green' 
+    };
+
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: window.Konva.Util.getRandomColor()
+    });
+  }
+
+  render() {
+    return (
+      <Rect x={10} y={10} width={50} height={50} fill={this.state.color} onClick={this.handleClick} draggable="true" />
+    );
+  }
+}
+
 
 class App extends Component {
   render() {
@@ -10,12 +35,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Stage width={900} height={900}>
+          <Layer>
+            <MyRect />
+          </Layer> 
+        </Stage>
+        <div>
+          Let's draw in Canvas!
+          </div>
       </div>
     );
   }
 }
+
 
 export default App;
